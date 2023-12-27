d86d1365bc12f15c77eca3ef32aab0e978d1118e
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2023-12-27 09:39:28 +0000

    Added probe point logic

--- src/base/pairing-validation.cpp
+++ src/base/pairing-validation.cpp
@@ -17,16 +17,93 @@ BasePairValidation::BasePairValidation(clipper::MiniMol& mol) {
 }
 
 void BasePairValidation::validate() {
+    clipper::MAtomNonBond ns = clipper::MAtomNonBond(m_mol, 5);
+
     for (int p = 0; p < m_mol.size(); p++) {
         for (int m = 0; m < m_mol[p].size(); m++) {
-            validate_basepair(m_mol[p][m]);
+            validate_basepair(m_mol[p][m], ns);
         }
     }
 }
 
-void BasePairValidation::validate_basepair(clipper::MMonomer& mon) {
+std::vector<std::string> BasePairValidation::find_bonded_h_atoms(const clipper::MMonomer&mon,
+                                                                 const std::string& donor_atom) {
+    std::vector<std::string> bonded_h_atoms = {};
+    std::string donor_h_atom = donor_atom;
+    donor_h_atom.replace(0,1, "H");
+
+    for (int i = 1; i <= 2; i++) {
+        std::string potential_h_atom = donor_h_atom + std::to_string(i);
+        int h_idx = mon.lookup(potential_h_atom, clipper::MM::UNIQUE);
+        if (h_idx >= 0) {bonded_h_atoms.push_back(potential_h_atom);}
+    }
+
+    return bonded_h_atoms;
+}
+
+clipper::Vec3<> BasePairValidation::get_probe_positon(clipper::MMonomer& mon, std::string const& donor_atom,
+                                                      const std::string& h_atom, double probe_length) noexcept {
+    const int donor_atom_idx = mon.lookup(donor_atom, clipper::MM::UNIQUE);
+    if (donor_atom_idx < 0) return clipper::Vec3<>::null();
+
+    const int h_atom_idx = mon.lookup(h_atom, clipper::MM::UNIQUE);
+    if (h_atom_idx < 0) return clipper::Vec3<>::null();
+
+    const clipper::Vec3<> bond_vector = mon[h_atom_idx].coord_orth() - mon[donor_atom_idx].coord_orth();
+    const clipper::Vec3<> probe_vector = bond_vector.unit() * probe_length;
+    const clipper::Vec3<> probe_position = mon[donor_atom_idx].coord_orth() + probe_vector;
+    return probe_position;
+}
+
+void BasePairValidation::validate_basepair(clipper::MMonomer& mon, clipper::MAtomNonBond ns) {
     HydrogenBondingAtoms h_atoms = ValidateUtil::get_h_atoms(mon.type());
 
+    std::cout << mon.type() << std::endl;
+    // Need to find the monomers which could possibly form a H bond to the H bond atoms.
+    clipper::MPolymer probe_chain;
+    probe_chain.set_id("P");
+    clipper::MMonomer probes;
+    probes.set_id(1);
+    probes.set_seqnum(1);
+    probes.set_type("X");
+
+    int l = 0;
+    for (auto const& donor_atom: h_atoms.donor_atoms) {
+        std::cout << donor_atom << std::endl;
+        std::vector<std::string> bonded_h_atoms = find_bonded_h_atoms(mon, donor_atom);
+        if (bonded_h_atoms.empty()) { std::cout << "No H atoms found on expected H bond donor, check hydrogenation\n";continue;}
+
+        for (auto const& h_atom: bonded_h_atoms) {
+            clipper::Vec3<> probe_position = get_probe_positon(mon, donor_atom, h_atom,2);
+            if (probe_position.is_null()) continue;
+            clipper::MAtom m;
+            m.set_id(l);
+            m.set_name("X");
+            m.set_element("X");
+            m.set_coord_orth(clipper::Coord_orth(probe_position));
+            m.set_u_iso(0);
+
+            l += 1;
+            probes.insert(m);
+
+            auto atoms_nearby = ns(clipper::Coord_orth(probe_position), 1);
+            for (auto const& atom_near: atoms_nearby) {
+                std::string residue_type = m_mol[atom_near.polymer()][atom_near.monomer()].type();
+                BasePairResult result = is_base_pair(mon.type(), residue_type);
+                if (result != pair) {continue;}
+
+                std::cout << mon.type() << "-" << residue_type << " link found" << std::endl;
+            }
+
+        }
+    }
+
+    probe_chain.insert(probes);
+    clipper::MiniMol xm = m_mol;
+    xm.insert(probe_chain);
+    clipper::MMDBfile mfile;
+    mfile.export_minimol(xm);
+    mfile.write_file("debug/1bna_probe_points.pdb");
 
 
 }
--- src/base/pairing-validation.h
+++ src/base/pairing-validation.h
@@ -3,8 +3,14 @@
 
 #ifndef BASE_PAIRING_VALIDATION_H
 #define BASE_PAIRING_VALIDATION_H
-#include "clipper/clipper-minimol.h"
 
+#include "clipper/clipper-minimol.h"
+#include <map>
+#include <set>
+
+enum BasePairResult {
+    pair, non_pair, not_recognised
+};
 
 /**
  * \brief BasePairValidation
@@ -17,11 +23,40 @@ public:
 
     void validate();
 
-    static void validate_basepair(clipper::MMonomer&mon);
+    static std::vector<std::string> find_bonded_h_atoms(const clipper::MMonomer&mon, const std::string&donor_atom);
+
+    static clipper::Vec3<> get_probe_positon(clipper::MMonomer& mon, const std::string &donor_atom,
+        const std::string& h_aton, double probe_length=3) noexcept;
+
+    void validate_basepair(clipper::MMonomer& mon, clipper::MAtomNonBond ns);
 
 private:
     clipper::MAtomNonBond m_ns;
     clipper::MiniMol m_mol;
+
+    BasePairResult is_base_pair(const std::string& a, const std::string& b) {
+        const auto a_it = base_pairing.find(a);
+        if (a_it == base_pairing.end()) {return not_recognised;}
+
+        const auto b_it = a_it->second.find(b);
+        if (b_it == a_it->second.end()) {return non_pair;}
+
+        return pair;
+
+    }
+
+    std::map<std::string, std::set<std::string>> base_pairing {
+        {"A", {"U", "DT"}},
+        {"DA", {"U", "DT"}},
+
+        {"DT", {"A", "DT"}},
+
+        {"C", {"G", "DG"}},
+        {"DC", {"G", "DG"}},
+
+        {"G", {"C", "DC"}},
+        {"DG", {"C", "DC"}}
+    };
 
 };
 
